890721e40af3485755bfcf28fa6218174a89e315
anonpekka <107747181+anonpekka@users.noreply.github.com>
2022-12-01 20:33:59 +0530

    commit to main

---
+++ index.js
@@ -0,0 +1,7 @@
+// JAVASCRIPT CODE ARE HERE FOR PAGE INTERACTION
+
+let Like = document.getElementById("like")
+
+Like.addEventListener("click", function(){
+    Like.innerHTML = "<img src='/images/heart.png' >"
+})
