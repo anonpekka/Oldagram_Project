9ab6b4f0de80ade2631deede141c2c7dd94ffa67
anonpekka <107747181+anonpekka@users.noreply.github.com>
2022-12-03 19:02:40 +0530

    update javascript

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ const likeTwo = document.getElementById("heart2-el")
 const likeThree = document.getElementById("heart3-el")
 let like = document.getElementById("like-el")
 let Num = 0
-let likeValue = like.innerText
-console.log(likeValue)
+Num = Num + 1
+let likeValue = parseInt(like.innerText)
+let aDd = likeValue + Num
+console.log(typeof likeValue)
 
 likeOne.addEventListener("click", function(){
-    likeOne.src = "/images/heart.png"
-    Num = Num + 1
-    let add = likeValue + Num
-    like.textContent = add
+    if (likeOne.src == "/images/icon-heart.png" ){
+        // likeOne.src = "/images/heart.png"
+        // like.textContent = aDd
+        console.log("yes")
+    } else {
+        // likeOne.src = "/images/icon-heart.png"
+        console.log("no")
+    }
 })
 
 likeTwo.addEventListener("click", function() {
@@ -23,3 +29,5 @@ likeThree.addEventListener("click", function(){
     likeThree.src = "/images/heart.png"
 
 })
+
+console.log(likeOne.src= "/images/icon-heart.png")
